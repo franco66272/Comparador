1e9cd744a4e56c060f080419a144d16d030ea3b3
franco66272 <138316538+franco66272@users.noreply.github.com>
2026-08-29 20:38:42 -0300

    Corregir extractor SCP Hardstore

--- extractores/scphardstore_com.py
+++ extractores/scphardstore_com.py
@@ -1,13 +1,11 @@
-"""Extractor automático para SCP Hardstore."""
-from .auto_generico import extraer_desde_config
+"""Extractor robusto para SCP Hardstore."""
+from .catalogo_robusto import extraer_desde_config_robusto
 
 CONFIG = {
     "tienda": "scphardstore_com",
     "url": "https://www.scphardstore.com",
-    "plataformas": ["woocommerce"],
     "catalog_urls": ["https://www.scphardstore.com/tienda/"],
-    "fuentes_prioritarias": [],
 }
 
 def extraer():
-    return extraer_desde_config(CONFIG)
+    return extraer_desde_config_robusto(CONFIG)
